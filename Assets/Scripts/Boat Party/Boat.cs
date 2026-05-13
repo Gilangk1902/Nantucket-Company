e89c651dcb49d3054f4e9387e8b4800ff838a28b
Gilang Kurniawan <90315796+Gilangk1902@users.noreply.github.com>
2026-05-13 10:57:33 +0700

    boat movement uses rigidbody

--- Assets/Scripts/Boat Party/Boat.cs
+++ Assets/Scripts/Boat Party/Boat.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
 public class Boat : MonoBehaviour
 {
     private string id;
-    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private float buoyancyForce;
+    [SerializeField] private float decelerationRate;
     [SerializeField] private string characterName;
-
+    
+    [SerializeField] private bool isMoving;
+    [SerializeField] private bool inWater;
+    [SerializeField] private Rigidbody rb;
+    private Vector3 targetPosition;
 
     private void Awake()
     {
@@ -23,6 +33,71 @@ public class Boat : MonoBehaviour
         return this.characterName;
     }
 
+    private void FixedUpdate()
+    {
+
+        if (!isMoving)
+        {
+            //rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, decelerationRate * Time.fixedDeltaTime);
+            if (inWater)
+            {
+                rb.AddForce(Vector3.up * buoyancyForce * Time.fixedDeltaTime, ForceMode.Force);
+                rb.drag = 3;
+            }
+            else
+            {
+                rb.drag = 0;
+            }
+        }
+        else
+        {
+            if (inWater)
+            {
+                rb.AddForce(Vector3.up * buoyancyForce * Time.fixedDeltaTime, ForceMode.Force);
+                rb.drag = 3;
+            }
+            else
+            {
+                rb.drag = 0;
+            }
+
+            // Direction to target
+            Vector3 direction = (targetPosition - transform.position);
+            direction.y = 0f;
+
+            // Stop if close enough
+            if (direction.magnitude < 2f) //TODO: adjust value
+            {
+                rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, decelerationRate * Time.fixedDeltaTime);
+                //rb.velocity = Vector3.zero;
+                isMoving = false;
+                
+            }
+            else
+            {
+                // Rotate toward target
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                Quaternion smoothRotation = Quaternion.Slerp(
+                    rb.rotation,
+                    targetRotation,
+                    rotationSpeed * Time.fixedDeltaTime
+                );
+
+                rb.MoveRotation(smoothRotation);
+
+                // Move forward
+                Vector3 moveDirection = transform.forward * moveSpeed;
+                rb.AddForce(new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z) * moveSpeed * Time.fixedDeltaTime, ForceMode.Force);
+                //rb.velocity = new Vector3(
+                //    moveDirection.x,
+                //    rb.velocity.y,
+                //    moveDirection.z
+                //);
+            }
+
+        }
+    }
+
     public static string GenerateID()
     {
         System.Text.StringBuilder sb = new System.Text.StringBuilder("C");
@@ -36,23 +111,31 @@ public class Boat : MonoBehaviour
 
     public void Move(Vector3 destination)
     {
-        agent.SetDestination(destination);
-        Debug.Log("move" + id);
+        targetPosition = destination;
+        isMoving = true;
     }
 
     public bool HasReachedDestination()
     {
-        float tolerance = 0.5f;
-        if (!agent.pathPending) 
-        {
-            if (agent.remainingDistance <= tolerance)
-            {
-                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
-                {
-                    return true;
-                }
-            }
-        }
         return false;
+    }
+
+    [SerializeField] private LayerMask waterLayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & waterLayer) != 0)
+        {
+            inWater = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & waterLayer) != 0)
+        {
+            inWater = false;
+        }
+    
     }
 }
--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -1,29 +1,112 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Ship : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent agent;
+    private bool isMoving;
+    private bool inWater;
+    
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private float buoyancyForce;
+    [SerializeField] private float decelerationRate;
+
+    [SerializeField] private Rigidbody rb;
+    private Vector3 targetPosition;
 
     public void Move(Vector3 destination)
     {
-        agent.SetDestination(destination);
+        targetPosition = destination;
+        isMoving = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isMoving)
+        {
+            //rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, decelerationRate * Time.fixedDeltaTime);
+            if (inWater)
+            {
+                rb.AddForce(Vector3.up * buoyancyForce * Time.fixedDeltaTime, ForceMode.Force);
+                rb.drag = 3;
+            }
+            else
+            {
+                rb.drag = 0;
+            }
+        }
+        else
+        {
+            if (inWater)
+            {
+                rb.AddForce(Vector3.up * buoyancyForce * Time.fixedDeltaTime, ForceMode.Force);
+                rb.drag = 3;
+            }
+            else
+            {
+                rb.drag = 0;
+            }
+
+            // Direction to target
+            Vector3 direction = (targetPosition - transform.position);
+            direction.y = 0f;
+
+            // Stop if close enough
+            if (direction.magnitude < 2f) //TODO: adjust value
+            {
+                rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, decelerationRate * Time.fixedDeltaTime);
+                //rb.velocity = Vector3.zero;
+                isMoving = false;
+
+            }
+            else
+            {
+                // Rotate toward target
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                Quaternion smoothRotation = Quaternion.Slerp(
+                    rb.rotation,
+                    targetRotation,
+                    rotationSpeed * Time.fixedDeltaTime
+                );
+
+                rb.MoveRotation(smoothRotation);
+
+                // Move forward
+                Vector3 moveDirection = transform.forward * moveSpeed;
+                rb.AddForce(new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z) * moveSpeed * Time.fixedDeltaTime, ForceMode.Force);
+                //rb.velocity = new Vector3(
+                //    moveDirection.x,
+                //    rb.velocity.y,
+                //    moveDirection.z
+                //);
+            }
+
+        }
     }
     public bool HasReachedDestination()
     {
-        float tolerance = 0.5f;
-        if (!agent.pathPending)
-        {
-            if (agent.remainingDistance <= tolerance)
-            {
-                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
-                {
-                    return true;
-                }
-            }
-        }
         return false;
+    }
+
+    [SerializeField] private LayerMask waterLayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & waterLayer) != 0)
+        {
+            inWater = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & waterLayer) != 0)
+        {
+            inWater = false;
+        }
+
     }
 }
